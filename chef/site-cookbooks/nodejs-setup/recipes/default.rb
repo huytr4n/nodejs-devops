4403ee32e1a7702909302c11b9fc224d820af2ce
Huy Tran <huy.tranquoc@asnet.com.vn>
2015-11-11 14:40:43 +0700

    Implement nodejs-setup site-cookbooks

---
+++ chef/site-cookbooks/nodejs-setup/recipes/default.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook Name:: nodejs-setup
+# Recipe:: default
+#
+# Copyright 2015, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+include_recipe 'nodejs-setup::install'
+include_recipe 'nodejs-setup::deploy'
