59d3ee679f6d7c179f4bbf5a86c9f46f12276fe2
Huy Tran <huy.tranquoc@asnet.com.vn>
2015-11-13 14:04:33 +0700

    Update server dashboard endpoint

--- backend/server/index.js
+++ backend/server/index.js
@@ -1,27 +1,112 @@
 /**
- * Main point of server
+ * Dashboard api server
  */
-var express = require('express');
-var config = require('../config/');
-var app = express();
-var port = config.get('app:port');
+'use strict';
 
-// dumpmy
-app.get('/dumpmy', function (req, res) {
-  return res.send({status: 200, message: 'This is dumpmy api'});
-});
+var assert = require('assert'),
+  http = require('http');
 
-// index
-app.get('/', function (req, res) {
-  return res.send({status: 200, message: 'Index page is simple'});
-});
+var async = require('async'),
+  express = require('express'),
+  methodOverride = require('method-override'),
+  bodyParser = require('body-parser'),
+  logger = require('morgan');
+
+var log = require('../utils/logger'),
+  ServiceBase = require('../utils/service'),
+  router = require('./router'),
+  DatabaseManager = require('../platform');
 
 /**
- * Expose.
+ * Web admin tool server
+ *
+ * @param {Configs} opts.configs
+ *      The configuration
+ *
+ * @return itself
  */
-module.exports = {
-  start: function (fn) {
-    console.log('start application at port', port);
-    app.listen(port, fn);
+var Server = ServiceBase.extend({
+
+  constructor: function (opts) {
+    opts = opts || {};
+
+    var self = this;
+    self.conf = opts.configs;
+
+    // Create express application
+    // --------------------------
+    var webapp = self.webapp = express();
+
+    // Define database manager
+    this.db = new DatabaseManager(this);
+
+    // webapp.use(express.logger());
+    // webapp.use(express.json());
+    // webapp.use(express.urlencoded());
+    webapp.use(logger('dev'));
+    webapp.use(bodyParser.json());
+    webapp.use(bodyParser.urlencoded({extended: false}));
+    webapp.use(methodOverride('X-HTTP-Method-Override'));
+
+    // CrossDomain middleware
+    var allowCrossDomain = function (req, res, next) {
+      res.header('Access-Control-Allow-Origin', '*');
+      res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+      res.header('Access-Control-Allow-Headers', 'Content-Type');
+      next();
+    };
+    webapp.use(allowCrossDomain);
+    // webapp.use(webapp);
+
+    // Config router
+    router(this);
+  },
+
+  /**
+   * Starts the service
+   */
+  onStart: function (callback) {
+    var self = this,
+      conf = this.conf;
+
+    async.parallel([
+      function initialDatabase(callback) {
+        log.info('Init Database service ...');
+        self.db.init([
+          'dbProduct'
+        ], function (err) {
+          if (err) {
+            return callback(err);
+          }
+
+          callback && callback();
+        });
+      },
+      function startAPIServer(callback) {
+        var port = conf.get('app:port');
+
+        log.info('Dashboard api started successfully at port: ', port);
+        self.httpServer = http.createServer(self.webapp).listen(port, callback);
+      }
+    ],
+    function (err, results) {
+      assert.ok(!err, 'E8884883388. Couldn\'t starts server');
+      self.onPreStart();
+      callback && callback(err, results);
+    });
+  },
+
+  onStop: function (callback) {
+    this.httpServer.close();
+    callback && callback();
+  },
+
+  // Event handlers from pubsub
+  // --------------------------
+
+  onPreStart: function () {
+    // TODO
   }
-};
+});
+
+module.exports = Server;
