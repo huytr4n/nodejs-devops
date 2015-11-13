fb5d3e9fe55ea990828d90a9c981e54e7ccf63d0
Huy Tran <huy.tranquoc@asnet.com.vn>
2015-11-13 15:30:20 +0700

    Add script start server before testing

---
+++ backend/test/index.js
@@ -0,0 +1,12 @@
+// Start server before test
+var DashboardServer = require('../server/');
+var config = require('../config');
+
+var dashboardServer =  new DashboardServer({
+  configs: config
+});
+
+// start server
+dashboardServer.start(function () {
+  console.log('server is ready for testing');
+});
