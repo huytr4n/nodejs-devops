0b7a7e4a2b590f6d21023e898f2e1096bfc95632
Huy Tran <huy.tranquoc@asnet.com.vn>
2015-11-13 14:03:34 +0700

    Add server test script

--- backend/test/server.js
+++ backend/test/server.js
@@ -1,26 +1,21 @@
 var Lab = require('lab');
 var lab = exports.lab = Lab.script();
 var http = require('http');
+var Code = require('code');
 
-var DUMMY_URL = 'http://127.0.0.1:9090/dumpmy';
-var INDEX_URL = 'http://127.0.0.1:9090/';
+var DUMMY_URL = 'http://localhost:9090/dumpmy';
+var INDEX_URL = 'http://localhost:9090/';
 
 lab.test('server is alive', function (done) {
   http.get(DUMMY_URL, function (res) {
-    if (res.statusCode === 200) {
-      done();
-    } else {
-      done({error: res.statusCode});
-    }
+    Code.expect(res.statusCode).to.equal(200);
+    done();
   });
 });
 
 lab.test('index is up', function (done) {
   http.get(INDEX_URL, function (res) {
-    if (res.statusCode === 200) {
-      done();
-    } else {
-      done({error: res.statusCode});
-    }
+    Code.expect(res.statusCode).to.equal(200);
+    done();
   });
 });
