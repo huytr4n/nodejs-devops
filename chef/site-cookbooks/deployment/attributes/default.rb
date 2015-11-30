b85d61210dbbcb5a5c3b1d2faff2659430eae5a9
Huy Tran <huy.tranquoc@asnet.com.vn>
2015-11-30 08:51:14 +0700

    Add option deploy with rsync

--- chef/site-cookbooks/deployment/attributes/default.rb
+++ chef/site-cookbooks/deployment/attributes/default.rb
@@ -3,3 +3,4 @@ default['deployment']['current'] = '/root/best-practice/current/'
 default['deployment']['sshkey'] = nil
 default['deployment']['git'] = 'https://github.com/tranquochuy/nodejs-devops.git'
 default['deployment']['branch'] = 'master'
+default['deployment']['rsync'] = false
--- chef/site-cookbooks/deployment/recipes/default.rb
+++ chef/site-cookbooks/deployment/recipes/default.rb
@@ -7,39 +7,41 @@
 # All rights reserved - Do Not Redistribute
 #
 
-# create folder
-directory node['deployment']['dir'] do
-  owner 'root'
-  group 'root'
-  mode '0755'
-  action :create
-  recursive true
-end
+if node['deployment']['rsync']
+  # create folder
+  directory node['deployment']['dir'] do
+    owner 'root'
+    group 'root'
+    mode '0755'
+    action :create
+    recursive true
+  end
 
-# copy ssh key
-if node['deployment']['sshkey']
-  owner 'root'
-  group 'root'
-  mode 0400
-  lazy { ::File.open(node['deployment']['sshkey']).read }
-  action :create
-end
+  # copy ssh key
+  if node['deployment']['sshkey']
+    owner 'root'
+    group 'root'
+    mode 0400
+    lazy { ::File.open(node['deployment']['sshkey']).read }
+    action :create
+  end
 
-# clone code
-deploy_branch node['deployment']['dir'] do
-  scm_provider Chef::Provider::Git
-  repo node['deployment']['git']
-  revision node['deployment']['branch']
+  # clone code
+  deploy_branch node['deployment']['dir'] do
+    scm_provider Chef::Provider::Git
+    repo node['deployment']['git']
+    revision node['deployment']['branch']
 
-  # Clear out all the symlink attributes
-  symlink_before_migrate.clear
-  create_dirs_before_symlink.clear
-  purge_before_symlink.clear
-  symlinks.clear
+    # Clear out all the symlink attributes
+    symlink_before_migrate.clear
+    create_dirs_before_symlink.clear
+    purge_before_symlink.clear
+    symlinks.clear
 
-  enable_submodules true
-  shallow_clone false
-  action :deploy
+    enable_submodules true
+    shallow_clone false
+    action :deploy
+  end
 end
 
 # start server
